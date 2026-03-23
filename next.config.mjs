7ec7f31b8c08533b9e740fa1e01df498792e1bbb
littlekidz <worasretkulkit@gmail.com>
2026-03-24 00:09:20 +0700

    fix url

--- next.config.mjs
+++ next.config.mjs
@@ -17,18 +17,19 @@ const nextConfig = {
     BACKEND_URL: process.env.BACKEND_URL,
   },
   async rewrites() {
+    const backend = process.env.BACKEND_URL || "http://localhost:5000";
     return [
       {
         source: "/api/providers/:path*",
-        destination: process.env.BACKEND_URL + "/api/providers/:path*",
+        destination: `${backend}/api/providers/:path*`,
       },
       {
         source: "/api/cars/:path*",
-        destination: process.env.BACKEND_URL + "/api/cars/:path*",
+        destination: `${backend}/api/cars/:path*`,
       },
       {
         source: "/api/rentals/:path*",
-        destination: process.env.BACKEND_URL + "/api/rentals/:path*",
+        destination: `${backend}/api/rentals/:path*`,
       },
     ];
   },
